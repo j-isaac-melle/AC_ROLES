36c5397989feadd982d716efd933807884d819a7
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 11:15:30 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -3,6 +3,14 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
 st.header('Air Canada Roles & Users')
 
 # Example 1

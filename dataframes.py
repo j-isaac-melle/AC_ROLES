466fd61f226f942c2f5c5336ef22dbe8aad749f8
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 12:25:31 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -3,18 +3,6 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title('Customizing the theme of Streamlit apps')
-
-st.write('Contents of the `.streamlit/config.toml` file of this app')
-
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#AED6F1"
-textColor="#2E86C1"
-font="monospace"
-""")
 
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)

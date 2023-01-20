ab1a3f45fcc631b4bd69b14b13ddc659450c59e6
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 12:47:42 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -138,29 +138,6 @@ st.line_chart(chart_data)
 
 # STREAMLIT LAYOUT: APP
 
-import streamlit as st
 
-st.set_page_config(layout="wide")
-
-st.title('How to layout your Streamlit app')
-
-with st.expander('About this app'):
-  st.write('This app shows the various ways on how you can layout your Streamlit app.')
-  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
-
-st.sidebar.header('Input')
-user_name = st.sidebar.text_input('What is your name?')
-user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
-user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
-
-st.header('Output')
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-  if user_name != '':
-    st.write(f'👋 Hello {user_name}!')
-  else:
-    st.write('👈  Please enter your **name**!')
 
 

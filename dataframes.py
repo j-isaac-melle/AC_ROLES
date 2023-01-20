dae3cc32b7be85f95810e2b985c07837c0d31981
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 12:16:36 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -1,15 +1,15 @@
-pip install streamlit_pandas_profiling
 import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
 
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+st.header('st.latex')
 
-st.header('`streamlit_pandas_profiling`')
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
 
 pr = df.profile_report()
 st_profile_report(pr)

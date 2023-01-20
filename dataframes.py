4d09e2c4fc911445db73afa45ecf9c4ea82b022b
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 12:29:15 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -42,6 +42,22 @@ df = pd.DataFrame({
 st.write(df)
 
 #Checkbox
+#FILE UPLOADER
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
+
+
 
 st.header('user/role checkbox')
 

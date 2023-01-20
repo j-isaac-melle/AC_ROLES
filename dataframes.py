daa8c371a894fa02ae280f92b449f4578f7455fd
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 10:31:07 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -1,5 +1,8 @@
-     
-import streamlit as st
+ import streamlit as st
 
-st.write('Hello world!')
+st.header('st.button')
 
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')

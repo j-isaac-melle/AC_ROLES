1719243b87f6f44cd739abaf96037456d7176b72
django <108687989+j-isaac-melle@users.noreply.github.com>
2023-01-20 12:42:54 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -3,6 +3,8 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+
+
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
 
@@ -134,5 +136,29 @@ st.line_chart(chart_data)
 
 #Favorite color selector:
 
+# STREAMLIT LAYOUT: APP
+
+st.set_page_config(layout="wide")
+
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
 
 
